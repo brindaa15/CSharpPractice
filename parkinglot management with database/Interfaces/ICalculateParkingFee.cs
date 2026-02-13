41055048114be0127da9a32e145c5afa5ead4f7b
B <bb630@apac.comcast.com>
2026-02-13 10:39:44 +0530

    Initial commit: Add ParkingLot_Management project with Database folder and SQL scripts

---
+++ parkinglot management with database/Interfaces/ICalculateParkingFee.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingLot_Management
+{
+    //Interface
+    // any class that implements this should calculate parking fee
+    interface ICalculateParkingFee
+    {
+        double CalculateFees();
+    }
+}
