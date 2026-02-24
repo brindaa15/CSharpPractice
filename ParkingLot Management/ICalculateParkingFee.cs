6f8065ddda45e4bd0671bdf502617c36e210683f
Brindaa B <brindaa.b04@gmail.com>
2026-02-24 10:14:33 +0530

    Delete ParkingLot Management directory

--- ParkingLot Management/ICalculateParkingFee.cs
+++
@@ -1,9 +0,0 @@
-namespace ParkingLot_Management
-{
-    // Interface
-    // any class that implements this should calculate parking fee
-    interface ICalculateParkingFee
-    {
-        double CalculateFees();
-    }
-}
